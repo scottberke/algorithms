fa3138aadb7849188ae907b550575acc73736c68
Scott B <scottberke@gmail.com>
2018-02-23 15:08:17 -0700

    Add interview question section. Add sorted merge

---
+++ sorting/interview_questions.md
@@ -0,0 +1,8 @@
+# Interview Questions - Sorting
+
+## Sorted Merge:
+Q) You're given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B. Write a method to merge B into A in sorted order.
+
+Solution: [Sorted Merge][./q_sorted_merge.py]
+
+Source: CTCI p.149
---
+++ sorting/q_sorted_merge.py
@@ -0,0 +1,65 @@
+# Sorted Merge
+
+# This method takes a 'python' approach where the array doesnt nessecarily
+# have a buffer but is dynamic in size.
+def merge(a_arr, b_arr):
+    # If either array is empty just concatenate them and return
+    if not a_arr or not b_arr:
+        return a_arr + b_arr
+    # Keep track of a_array index
+    a_index = 0
+    # Go through all b_arr elements
+    for b_index, b_val in enumerate(b_arr):
+        # Increment a_index until its value > b_val
+        while b_val > a_arr[a_index]:
+            a_index += 1
+            # If were at the end of a_arr, just concat b_arr and return
+            # since we know all remaining b_vals go at the end
+            if a_index == len(a_arr):
+                return a_arr + b_arr[b_index:]
+
+        # Insert b_val into a where it belongs
+        a.insert(a_index, b_val)
+
+    return a_arr
+
+a = [1, 3, 5, 7, 9, 11, 32, 41]
+b = [2, 4, 6, 8, 15, 25, 52]
+print(merge(a, b))
+
+
+# This approach assumes that the a array is actually fixed size with an
+# adequate buffer to hold all of b array
+def merge(a_arr, b_arr):
+    # If either array is empty just concatenate them and return
+    if not a_arr or not b_arr:
+        return a_arr + b_arr
+
+    # Keep track of our indices and a_arrays end index
+    b_index = len(b_arr) - 1
+    a_index = len(a_arr) - 1
+    a_end = len(a_arr) - 1
+
+    # Work backwards so we dont have to shift the entire array
+    while a_index >= 0 and b_index >= 0:
+        # Decrement a_index until were out of the buffer
+        if not a_arr[a_index]:
+            a_index -= 1
+            continue
+        # Put a's value at the end if its larger and decrement a's index
+        if a_arr[a_index] > b_arr[b_index]:
+               a_arr[a_end] = a_arr[a_index]
+               a_index -= 1
+        # Otherwise, b's value is larger so put it at the end and
+        # decrement b's index
+        else:
+            a_arr[a_end] = b_arr[b_index]
+            b_index -= 1
+
+        # Update our end index
+        a_end -= 1
+
+a = [1, 3, 5, 7, None, None, None, None]
+b = [2, 4, 6, 8]
+merge(a, b)
+print(a)
