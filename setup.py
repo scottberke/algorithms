ed3f9df0bf23525c8369213f3ffd64f918f234bc
Scott B <scottberke@gmail.com>
2018-04-18 14:03:38 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algorithms',
+    version='0.1.0',
+    description='Collection of helpful algorithms and related CS topics.',
+    author='Scott Berke',
+    author_email='scottberke@gmail.com',
+    packages=find_packages()
+)
